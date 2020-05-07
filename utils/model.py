3700ea597d4ffb1d7b1b9f14539fe98c51dd2096
mysterio42 <bachana3435@gmail.com>
2020-05-07 20:10:15 +0400

    utils for model

---
+++ utils/model.py
@@ -0,0 +1,21 @@
+from pybrain.structure.modules import SoftmaxLayer
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.utilities import percentError
+
+
+def train_model(train_data, test_data, epochs=1000, hidden_dim=7, lr=0.05, momentum=0.01):
+    net = buildNetwork(train_data.indim, hidden_dim, train_data.outdim, outclass=SoftmaxLayer)
+    print(net)
+    input('---------')
+    trainer = BackpropTrainer(net, dataset=train_data, momentum=momentum, learningrate=lr, verbose=True)
+    trainer.trainEpochs(epochs)
+
+    print(f" Test data error: {percentError(trainer.testOnClassData(dataset=test_data), test_data['class'])}")
+
+    return net
+
+
+def predict_model(net, features):
+    for i, feature in enumerate(features):
+        print(f'{feature} - {net.activate(feature)} ')
