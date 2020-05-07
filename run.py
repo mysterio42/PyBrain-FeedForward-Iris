f8d307bf84d770e4996a5631a32faac45f01f401
mysterio42 <bachana3435@gmail.com>
2020-05-07 20:11:59 +0400

    add run module for dev

--- run.py
+++ run.py
@@ -5,15 +5,6 @@ from utils.model import train_model, predict_model
 
 
 def parse_args():
-    def str2bool(v):
-        if isinstance(v, bool):
-            return v
-        if v.lower() in ('yes', 'true', 't', 'y', '1'):
-            return True
-        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-            return False
-        else:
-            raise argparse.ArgumentTypeError('Boolean value expected')
 
     parser = argparse.ArgumentParser()
 
